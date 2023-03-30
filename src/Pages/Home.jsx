2acdf14b9bffda656661cceb473a0c3d657b2f90
raobaba <shrikishunr7@gmail.com>
2023-03-30 16:17:05 +0530

    create assest for image

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import {Box,styled} from "@mui/material";
+import SunSet from "../Assests/Images/bg.jpg";
+const Component = styled(Box)({
+    height:'100vh'
+})
+const Image = styled(Box)({
+    backgroundImage:`url(${SunSet})`,
+    width:'27%',
+    height:'80%',
+    backgroundSize:'position'
+})
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <Component>
+       <Image></Image>
+       <Box></Box>
+    </Component>
   )
 }
