82ac94452900bf030c4df05b3764bc2e6cb042fc
raobaba <shrikishunr7@gmail.com>
2023-03-30 16:51:40 +0530

    form component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import {Box,styled} from "@mui/material";
 import SunSet from "../Assests/Images/bg.jpg";
 const Component = styled(Box)({
-    height:'100vh'
+    height:'100vh',
+    display:'flex',
+    alignItems:'center',
+    margin:'0 auto',
+    width:'65%'
 })
 const Image = styled(Box)({
     backgroundImage:`url(${SunSet})`,
     width:'27%',
     height:'80%',
-    backgroundSize:'position'
+    backgroundSize:'position',
+    borderRadius:'20px 0 0 20px'
 })
 export default function Home() {
   return (
